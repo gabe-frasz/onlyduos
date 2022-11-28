79d9641dc537ebe4a5c6fb2fa96bd3250136cdff
Gabriel <gabrielvitor.frasao@gmail.com>
2022-11-28 20:52:59 -0300

    feat(prisma): add prisma setup

--- apps/server/src/routes.ts
+++ apps/server/src/routes.ts
@@ -1,8 +1,21 @@
 import express, { Request, Response } from "express";
+import { prisma } from "./services";
 
 export const routes = express.Router();
 
-routes.route("/games").get((req: Request, res: Response) => {});
+routes.route("/games").get(async (req: Request, res: Response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  return res.status(200).json(games);
+});
 
 routes.route("/games/:id/ads").get((req: Request, res: Response) => {});
 
